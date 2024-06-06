78355468833177901665a1943929c952d46c46bd
renancassi <renanvic7@gmail.com>
2024-06-06 17:48:59 -0300

    bairros dv

--- gerador_cadastro.py
+++ gerador_cadastro.py
@@ -1,5 +1,6 @@
 from faker import Faker
 import pandas as pd
+import random
 
 fake_geral = Faker()
 fake_cpf = Faker('pt_BR')
@@ -8,7 +9,24 @@ fake_cpf = Faker('pt_BR')
 fake_cep = '85660000'
 fake_cidade = 'Dois Vizinhos - PR'
 
-
+bairros = [
+    "Da Luz",
+    "Das Torres",
+    "Sagrada Família",
+    "Santo Antônio",
+    "Jardim da colina",
+    "Jardim Marcante",
+    "São Francisco de Assis",
+    "Esperança",
+    "Santa Luzia",
+    "Centro Sul",
+    "Centro",
+    "Centro Norte",
+    "São Francisco Xavier",
+    "Alto da Colina",
+    "São Judas Tadeu",
+    "Vitória"
+]
 
 data = [(
     idx + 1,
@@ -18,8 +36,9 @@ data = [(
     fake_cep,
     fake_cidade,
     fake_cpf.street_name(),
-    fake_cpf.building_number())
-    for idx in range(50)] # Se quiser gerar mais que 50, troca esse 50 xD
+    fake_cpf.building_number(),
+    random.choice(bairros)
+    ) for idx in range(50)] # Se quiser gerar mais que 50, troca esse 50 xD
 
 
 df = pd.DataFrame(data, columns=[
@@ -30,7 +49,8 @@ df = pd.DataFrame(data, columns=[
     'cliente_end_cep',
     'cliente_end_cidade',
     'cliente_end_rua',
-    'cliente_end_numero'])
+    'cliente_end_numero',
+    'cliente_end_bairro'])
 
 
 df.to_excel("nomes_cpfs_enderecos_registro.xlsx", index=False)
